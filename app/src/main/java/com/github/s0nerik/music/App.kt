76f1a5ef1cd56906c6ee305dace748f1cd8531df
Alex Isaienko <thesonerik@gmail.com>
2016-09-25 02:13:23 +0300

    GlideBindingAdapter

--- app/src/main/java/com/github/s0nerik/music/App.kt
+++ app/src/main/java/com/github/s0nerik/music/App.kt
@@ -5,6 +5,7 @@ import com.chibatching.kotpref.Kotpref
 import com.github.s0nerik.music.di.AppComponent
 import com.github.s0nerik.music.di.AppModule
 import com.github.s0nerik.music.di.DaggerAppComponent
+import com.github.s0nerik.music.glide_bindingadapter.GlideBindingConfig
 import ru.noties.debug.Debug
 
 class App : Application() {
@@ -12,6 +13,8 @@ class App : Application() {
         comp = DaggerAppComponent.builder()
                 .appModule(AppModule(this))
                 .build()
+
+        registerGlideConfigs()
     }
 
     companion object {
@@ -22,5 +25,22 @@ class App : Application() {
         super.onCreate()
         Debug.init(BuildConfig.DEBUG)
         Kotpref.init(this)
+    }
+
+    private fun registerGlideConfigs() {
+        GlideBindingConfig.registerProvider("playbackBg", {
+            it.centerCrop()
+                    .placeholder(R.color.md_black_1000)
+                    .error(R.drawable.no_cover)
+                    .skipMemoryCache(true)
+        })
+
+        GlideBindingConfig.registerProvider("song", {
+            it.centerCrop()
+                    .placeholder(R.color.md_black_1000)
+                    .error(R.drawable.no_cover)
+        })
+
+        GlideBindingConfig.setDefault("song")
     }
 }
