f2d036656eb6fb95247fac562af369d6ca2c0c3a
Alex Isaienko <thesonerik@gmail.com>
2016-10-16 01:35:30 +0300

    PlayerController

---
+++ app/src/main/java/com/github/s0nerik/music/players/PlayerController.kt
@@ -0,0 +1,17 @@
+package com.github.s0nerik.music.players
+
+import com.github.s0nerik.music.ext.addTo
+import rx.subscriptions.CompositeSubscription
+
+class PlayerController(
+        val player: BasePlayer,
+        private val disposable: CompositeSubscription = CompositeSubscription()
+) {
+    fun play() = player.setPaused(false).subscribe().addTo(disposable)
+    fun pause() = player.setPaused(true).subscribe().addTo(disposable)
+    fun togglePause() = player.togglePause().subscribe().addTo(disposable)
+    fun playNext() = player.playNextSong().subscribe().addTo(disposable)
+    fun playPrev() = player.playPrevSong().subscribe().addTo(disposable)
+    fun seekTo(ms: Long) = player.seekTo(ms).subscribe().addTo(disposable)
+    fun dispose() = disposable.clear()
+}
