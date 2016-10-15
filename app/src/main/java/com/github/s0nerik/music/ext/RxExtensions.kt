5a3a6148b32c739f4c15c2bdb61ccf30b9544825
Alex Isaienko <thesonerik@gmail.com>
2016-10-16 01:34:54 +0300

    addTo extension for Subscription

--- app/src/main/java/com/github/s0nerik/music/ext/RxExtensions.kt
+++ app/src/main/java/com/github/s0nerik/music/ext/RxExtensions.kt
@@ -1,8 +1,15 @@
 package com.github.s0nerik.music.ext
 
 import rx.Observable
+import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
+import rx.subscriptions.CompositeSubscription
 
 fun <T> Observable<T>.observeOnMainThread(): Observable<T> {
     return observeOn(AndroidSchedulers.mainThread())
+}
+
+fun Subscription.addTo(subscription: CompositeSubscription): Subscription {
+    subscription.add(subscription)
+    return this
 }
