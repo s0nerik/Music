d80c392ad11b4992363cb06e896d8a8ae8291ba0
Alex Isaienko <thesonerik@gmail.com>
2016-10-17 02:45:09 +0300

    Fixed hang on permission request when starting app with turned off screen

--- app/src/main/java/com/github/s0nerik/music/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/github/s0nerik/music/screens/splash/SplashActivity.kt
@@ -5,25 +5,38 @@ import android.os.Bundle
 import com.github.s0nerik.music.App
 import com.github.s0nerik.music.base.BaseActivity
 import com.github.s0nerik.music.data.helpers.CollectionManager
+import com.github.s0nerik.music.ext.observeOnMainThread
 import com.github.s0nerik.music.screens.main.MainActivity
 import com.tbruyelle.rxpermissions.RxPermissions
 import com.trello.rxlifecycle.kotlin.bindToLifecycle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
 import org.jetbrains.anko.startActivity
 import rx.schedulers.Schedulers
 import javax.inject.Inject
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(), AnkoLogger {
     @Inject lateinit var collectionManager: CollectionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         App.comp.inject(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initCollection()
+    }
+
+    fun initCollection() {
         RxPermissions.getInstance(this)
                 .request(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .flatMap { collectionManager.initFromMediaStore() }
                 .bindToLifecycle(this)
                 .subscribeOn(Schedulers.io())
+                .observeOnMainThread()
                 .subscribe {
+                    debug("initFromMediaStore::onNext")
                     startActivity<MainActivity>()
                     finish()
                 }
